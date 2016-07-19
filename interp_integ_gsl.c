31ed2324abe9e30874ec2f4d2af2474605d390c8
Darivadi <darivadi@gmail.com>
2016-07-19 16:56:47 -0500

    19-06-16 16:56

--- interp_integ_gsl.c
+++ interp_integ_gsl.c
@@ -83,12 +83,20 @@ double *dT_dr_gsl(int i, int j)
       if( k != (GV.NCELLS-1) )
 	{
 	  T_depth[k] = interp_integ_potdot_dx(gp[m].pos[Z]-GV.CellStep, GV.BoxSize); 
+	  
+	  
+	  
 	  DeltaT[k] = T_depth[k+1] - T_depth[k];
 	}//if
       else
 	{
 	  DeltaT[k] = T_depth[k] = gp[m].potDot_r; 	  
 	}//else
+      
+      if(m == 0 || m == 256)
+	    {
+	      printf("m=%d, k=%d, T= %lf\n", m, k, T_depth[k]);
+	    }//if
       
       dr = GV.BoxSize / (1.0 * GV.NCELLS); 
       dT_dr[k] = DeltaT[k] / dr ;
